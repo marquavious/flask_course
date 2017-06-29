e662e3ae573f4e4349bc626b039365fe69373612
Marquavious Draggon <marq.draggon@gmail.com>
2017-06-28 23:18:21 -0400

    refactored

--- code/models/user.py
+++ code/models/user.py
@@ -1,7 +1,6 @@
 import sqlite3
-from flask_restful import Resource, reqparse
 
-class User:
+class UserModel:
     def __init__(self,_id,username,password):
         self.id = _id
         self.username = username
@@ -25,7 +24,7 @@ class User:
         return user
 
     @classmethod
-    def find_by_userid(cls,username):
+    def find_by_userid(cls,_id):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -40,27 +39,3 @@ class User:
 
         connection.close()
         return user
-
-class UserRegister(Resource):
-
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-        type = str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    parser.add_argument('password',
-        type = str,
-        required=True,
-        help="This field cannot be left blank!"
-    )
-    def post(self):
-        data = UserRegister.parser.parse_args()
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "INSERT INTO users VALUES(NULL,?,?)"
-        cursor.execute(query,(data['username'],data['password']))
-        connection.commit()
-        connection.close()
-
-        return {"message":"User created sucessfuly"}, 201
